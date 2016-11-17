a4ace8584540117b8128c8bfac9d48f8edfe012b
Ramaraja-Ramanujan <r.ramaraja@yahoo.com>
2016-11-17 21:00:14 +0530

    Changed Date

--- src/com/nexus/sample/java/serviceimpl/HistoryServiceImpl.java
+++ src/com/nexus/sample/java/serviceimpl/HistoryServiceImpl.java
@@ -17,7 +17,7 @@ public class HistoryServiceImpl implements HistoryService {
 
 	public History getHistory() {
 		Client client = Client.create();
-		WebResource webResource = client.resource("http://history.muffinlabs.com/date/11/17");
+		WebResource webResource = client.resource("http://history.muffinlabs.com/date/01/01");
 		ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
 		String output = response.getEntity(String.class);
 
