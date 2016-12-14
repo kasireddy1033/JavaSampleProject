4d1be9913c9bb7d751f09112d23045f361341d6c
Ramaraja-Ramanujan <r.ramaraja@yahoo.com>
2016-12-14 11:38:03 +0530

    Update Application.java
    New line

--- src/com/nexus/sample/Application.java
+++ src/com/nexus/sample/Application.java
@@ -1,6 +1,5 @@
 package com.nexus.sample;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
