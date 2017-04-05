b0c4202cde9036ae53a1b1ae41793d105e639b4d
bobanabraham <boban.abraham@gmail.com>
2017-04-05 10:26:53 +0530

    Update Application.java

--- src/com/nexus/sample/Application.java
+++ src/com/nexus/sample/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-//import java.lang.Exception;
+import java.lang.Exception;
 
 
 @SuppressWarnings("deprecation")
