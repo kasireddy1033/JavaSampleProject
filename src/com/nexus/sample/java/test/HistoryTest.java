2010e416e1101d1221116ed2b8650fb3dc778d18
Ramaraja Ramanujan <ramaraja.r@mindtree.com>
2017-07-13 11:56:21 +0530

    Added a simple crude unit test case

---
+++ src/com/nexus/sample/java/test/HistoryTest.java
@@ -0,0 +1,14 @@
+package com.nexus.sample.java.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HistoryTest {
+
+	@Test
+	public void test() {
+	//	fail("Not yet implemented");
+	}
+
+}
